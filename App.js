c3ef72b933de296a2fc864ea618b0a1823142422
rhonvyramos <rhonvyramos@gmail.com>
2023-10-07 12:36:57 -0400

    App, index, and template js react file skeletons created

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Template from "./components/template.js";
+
+function App() {
+    return <Template />;
+  }
+  
+  export default App;
+  
---
+++ components/template.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Template(props) {
+  return (
+    <h1>Hello!</h1>
+  );
+}
+
+export default Template;
